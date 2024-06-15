0aaf4bc0450323ee2beb1624f8361f6bf51656a5
muhammadasifrajpt <muhmammadasifrajput5@gmail.com>
2024-06-15 04:51:49 -0700

    Grade_check

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import promptSync from "prompt-sync";
+const prompt = promptSync();
+let marks = parseInt(prompt('Enter Marks To Get Result :'));
+let result = "";
+if (marks >= 80 && marks <= 99) {
+    console.log("Your Grade Is 'A'");
+}
+else if (marks >= 70 && marks <= 80) {
+    console.log("Yor Grade is 'B'");
+}
+else if (marks >= 55 && marks <= 70) {
+    console.log("Your Grade Is 'C'");
+}
+else if (marks >= 40 && marks <= 55) {
+    console.log("Your Grade Is 'D'");
+}
+else if (marks >= 15 && marks <= 40) {
+    console.log("'You Are Fail'");
+}
+else {
+    console.log("Sorry");
+}
+console.log(result);
